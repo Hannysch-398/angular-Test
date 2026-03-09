07f83134b00e83a368878cde9d9d5ee81eea77bf
dylan.schwarz <dylan.schwarz@hmmh.de>
2026-03-09 16:26:48 +0100

    - added effect for console log for the likes of a post. - added a signal for displaying the combination of all likes - added visual indicator for the most liked post

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input, InputSignal} from '@angular/core';
+import {Component, effect, HostBinding, inject, input, InputSignal} from '@angular/core';
 import {Post} from '../interface/post';
 import {PostService} from '../services/post.service';
 
@@ -11,8 +11,20 @@ import {PostService} from '../services/post.service';
 export class BlogDetailComponent {
 
   posts: InputSignal<Post>= input.required<Post>();
-
+  mostLiked = input.required<boolean>();
   postService = inject(PostService);
+
+  @HostBinding('class.mostLiked')
+  get mostLikedClass(): boolean {
+    return this.mostLiked();
+  }
+
+  constructor() {
+    effect(() => {
+      console.log(`Post: ${this.posts().id} | Likes: ${this.posts().likes}`);
+    });
+  }
+
 
 //  like(postId: number): void {
 //    this.postService.likePost(postId);
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, inject, Signal} from '@angular/core';
+import {Component, OnDestroy, OnInit, inject, Signal, computed} from '@angular/core';
 import {Post} from '../interface/post';
 import {BlogDetailComponent} from '../blog-detail/blog-detail.component';
 import {PostService} from '../services/post.service';
@@ -13,6 +13,7 @@ import {PostService} from '../services/post.service';
 })
 export class BlogComponent implements OnInit ,OnDestroy{
 
+
   ngOnDestroy() :void{
     console.log('Component ngOnDestroy');
   }
@@ -23,5 +24,23 @@ export class BlogComponent implements OnInit ,OnDestroy{
   postService = inject(PostService);
 
   posts: Signal<Post[]> = this.postService.getAllPosts();
+
+  allLikes: Signal<number> = computed(() =>
+    this.posts().reduce((sum, post) => sum + (post.likes ?? 0), 0)
+  );
+
+  mostLikedPost: Signal<number> = computed(() => {
+    let maxLikes = 0;
+    let mostLikedIndex = 0;
+    for (let i = 0; i < this.posts().length; i++) {
+      const currentLikes = this.posts()[i].likes ?? 0;
+      if (currentLikes > maxLikes) {
+        maxLikes = currentLikes;
+        mostLikedIndex = i+1;
+      }
+    }
+    return mostLikedIndex;
+  });
+
 
 }
